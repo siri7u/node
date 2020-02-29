d2db6bd69a8f7cbf3b7565c64ece7538f6649c63
siri7u <romaric.siri2@etu.univ-lorraine.fr>
2020-02-29 18:42:16 +0100

    test

--- server.js
+++ server.js
@@ -12,9 +12,23 @@ const server = express()
 
 const io = socketIO(server);
 
+
+const players = [];
+
+
 io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
+
+
+    //quand un évenement de souris est reçu
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
+
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
